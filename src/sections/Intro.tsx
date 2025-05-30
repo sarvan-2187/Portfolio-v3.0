9ca93ba982b9c615549804a19b405e4435a85da7
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-05-30 19:04:03 +0530

    Update Intro.tsx

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -30,7 +30,7 @@ const Intro: FC = () => {
       )
     }
   }, [inView, animate, scope]);
-  return <section id="about" className="py-20 md:py-32 mt-16 md:mt-16 lg:py-38 lg:mt-16" ref={scope}>
+  return <section id="about" className="py-24 md:py-32 mt-16 md:mt-16 lg:py-38 lg:mt-16" ref={scope}>
     <div className="container">
       <h2 className="text-4xl md:text-7xl lg:text-6.5xl lg:w-{95%} ">I build visually striking, high-performance websites with clean, scalable code and thoughtful, user-focused design. My goal is to help your business grow, connect with your audience, and stand out in today’s competitive digital landscape.</h2>
     </div>
