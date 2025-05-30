c63a595fb55fdc23b78187f3228323a5283a4dfa
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-05-30 18:56:19 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -98,7 +98,7 @@ const Hero: FC = () => {
               alt="My Portrait"
             />
           </motion.div>
-          <div className="w-full pt-14">
+          <div className="md:hidden lg:hidden w-full mt-18">
           <Image
               src={heroImage}
               className="md:hidden lg:hidden size-full object-cover"
