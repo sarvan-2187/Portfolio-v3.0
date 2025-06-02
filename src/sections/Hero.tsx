8497c25217bfda72298d64de080a59bad392aacd
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-02 15:14:08 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -45,7 +45,7 @@ const Hero: FC = () => {
               className="text-5xl md:text-6xl sm:max-w-[90%]  mt-40 md:mt-0 lg:text-7xl md:w-[80%]"
             >
               Hola!👋 I'm Sarvan Kumar. My Core TechStacks are HTML, CSS, JavaScript, React, Next.JS, Tailwind CSS</motion.h1>
-            <div className="flex flex-col mt-10 items-start gap-6 md:flex md:flex-row md:items-center">
+            <div className="flex flex-col mt-10 mb-10 items-start gap-6 md:flex md:flex-row md:items-center">
               <motion.div
                 initial={{ opacity: 0, y: '100%' }}
                 animate={{ opacity: 1, y: 0 }}
