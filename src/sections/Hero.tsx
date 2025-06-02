7513bd0e40809378583497785a00bc70a5988e08
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-02 15:09:12 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -44,7 +44,7 @@ const Hero: FC = () => {
               ref={titleScope}
               className="text-5xl md:text-6xl sm:max-w-[90%]  mt-40 md:mt-0 lg:text-7xl md:w-[80%]"
             >
-              Hey! I'm Sarvan Kumar. My Core TechStacks are HTML, CSS, JavaScript, React, Next.JS</motion.h1>
+              Hola!👋 I'm Sarvan Kumar. My Core TechStacks are HTML, CSS, JavaScript, React, Next.JS</motion.h1>
             <div className="flex flex-col mt-10 items-start gap-6 md:flex md:flex-row md:items-center">
               <motion.div
                 initial={{ opacity: 0, y: '100%' }}
