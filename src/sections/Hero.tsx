56d4170aea06939191604d98597f2057a6183a55
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-02 15:25:24 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -42,7 +42,7 @@ const Hero: FC = () => {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               ref={titleScope}
-              className="text-4xl md:text-6xl sm:max-w-[90%]  mt-40 md:mt-0 lg:text-7xl md:w-[80%]"
+              className="text-4xl md:text-6xl sm:max-w-[95%]  mt-40 md:mt-0 lg:text-7xl md:w-[80%]"
             >
               Hey! I'm Sarvan Kumar. My Core TechStacks are HTML, CSS, JavaScript, React, Next.JS, Tailwind CSS, Framer-Motion.</motion.h1>
             <div className="flex flex-col mt-10 mb-10 items-start gap-6 md:flex md:flex-row md:items-center">
