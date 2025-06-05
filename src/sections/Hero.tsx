290c411f6804ff6c2510abebdc53ef45ee7fe9f2
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-05 11:26:00 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -38,7 +38,7 @@ const Hero: FC = () => {
         {/* Left Side */}
         <div className="md:col-span-7 flex flex-col justify-center">
           <div className="container !max-w-full sm:h-screen">
-            <div className="flex items-center justify-center md:hidden lg:hidden mt-10 w-[100px] h-[100px]">
+            <div className="flex items-center justify-center md:hidden lg:hidden mt-20 w-[100px] h-[100px]">
           <Image
               src={heroImage}
               className="md:hidden lg:hidden size-full object-cover rounded-full"
