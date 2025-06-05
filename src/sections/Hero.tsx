b08c11d4084897d2d5ded10d5b02b7fdd1bd757f
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-05 10:59:59 +0530

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -38,7 +38,7 @@ const Hero: FC = () => {
         {/* Left Side */}
         <div className="md:col-span-7 flex flex-col justify-center">
           <div className="container !max-w-full sm:h-screen">
-            <h1 className="text-4xl md:text-6xl sm:max-w-[95%] mt-60 md:mt-40  mb-4 lg:text-5xl md:w-[80%]"
+            <h1 className="text-4xl md:text-6xl sm:max-w-[95%] mt-40 md:mt-40  mb-4 lg:text-5xl md:w-[80%]"
 >Welcome! <span className='wave'>👋</span><br></br>I&apos;m <span className='text-gray-200 animate-pulse'>Sarvan Kumar</span></h1>
             <motion.h1
               initial={{ opacity: 0 }}
