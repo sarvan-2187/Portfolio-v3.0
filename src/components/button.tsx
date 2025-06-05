0cba63644a90ee18011f26d125e12aa288a7caf1
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-05 10:45:12 +0530

    Update button.tsx

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,7 +7,7 @@ const Button = (props: {
 } & ButtonHTMLAttributes<HTMLButtonElement>) => {
     const {className, children,variant, iconAfter, ...rest} = props;
     return(
-        <button className={twMerge("h-11 px-6 border border-orange-500 uppercase rounded-xl inline-flex group/button  items-center gap-2", variant === "primary" && "bg-orange-500 text-white",variant === "secondary" && "",variant === "text" && "h-auto px-0 border-transparent",className)} {...rest}>
+        <button className={twMerge("h-11 px-6 border border-gray-500 uppercase rounded-xl inline-flex group/button  items-center gap-2", variant === "primary" && "bg-gray-500 text-white",variant === "secondary" && "",variant === "text" && "h-auto px-0 border-transparent",className)} {...rest}>
             <span>{children}</span>
             {iconAfter && <span>{iconAfter}</span>}
         </button>
