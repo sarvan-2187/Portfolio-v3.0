054c20e825bb947768ecb643975ac5e27dc333bf
Nagarampalli Sarvan Kumar <sarvankumarnagarampalli478@gmail.com>
2025-06-05 11:28:47 +0530

    Update certifications.tsx

--- src/sections/certifications.tsx
+++ src/sections/certifications.tsx
@@ -3,10 +3,7 @@
 import Image from "next/image";
 import { FC, useEffect, useRef } from "react";
 import gallery1 from "@/assets/images/cert-1.png";
-import gallery2 from "@/assets/images/cert-2.jpg";
 import gallery3 from "@/assets/images/cert-3.png";
-import gallery4 from "@/assets/images/cert-4.png";
-import gallery5 from "@/assets/images/cert-5.png";
 import gallery6 from "@/assets/images/cert-6.png";
 
 
@@ -20,30 +17,10 @@ const galleryItems = [
     href:'https://app-na2.hubspot.com/academy/achievements/j9ykrpsp/en/1/nagarampalli-sarvan-kumar/seo',
   },
   {
-    image: gallery2,
-    title: "Academic Excellence Award",
-    description: "The award was presented by the Vice-Chancellor of Andhra University for academic excellence among top-performing students.",
-    href:'https://app-na2.hubspot.com/academy/achievements/j9ykrpsp/en/1/nagarampalli-sarvan-kumar/seo',
-  },
-  {
     image: gallery3,
     title: "SEO Certified",
     description: "Successfully cleared the Search Engine Optimization Certification exam with a score of 50/60 from Hubspot Academy.",
         href:'https://app-na2.hubspot.com/academy/achievements/j9ykrpsp/en/1/nagarampalli-sarvan-kumar/seo',
-
-  },
-  {
-    image: gallery4,
-    title: "Canva Essentials",
-    description: "Successfully cleared the Canva Essentials Certification exam with a score of 20/20 from Canva.",
-        href:'https://www.canva.com/design-school/certification-award/5328e5d4-ee86-4999-90ca-0783ad2a2c71',
-
-  },
-  {
-    image: gallery5,
-    title: "Postman API Fundamentals Expert",
-    description: "Successfully cleared all the tasks in Postman and earned the Postman API Fundamentals Expert Badge",
-        href:'https://badgr.com/public/assertions/xkHwp2KZRIOayjIDMuJyHg',
 
   },
   {
